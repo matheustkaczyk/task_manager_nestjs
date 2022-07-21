36879303219387adf0ce4081a0e0ff0cd8a1f792
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 10:16:01 -0300

    feat: gerando controller do company

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompanyController } from './company/company.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CompanyController],
   providers: [AppService],
 })
 export class AppModule {}
