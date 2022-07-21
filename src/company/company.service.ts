3de3572035266ab9c857deed702d6275dd08df0b
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 10:49:16 -0300

    feat: injetando no company service o model

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Company, CompanyDocument } from './schemas/company.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(@InjectModel(Company.name) private companyModel: Model<CompanyDocument>) {}
+
+  async createCompany(): Promise<void> {
+    
+  }
+}
