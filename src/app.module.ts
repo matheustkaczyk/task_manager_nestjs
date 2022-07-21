ab06749fceb45603d11feea939653ecd89c2d1d2
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 11:31:46 -0300

    fix: corrigindo erro no app module, tirando companyController e CompanyService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompanyController } from './company/company.controller';
-import { CompanyService } from './company/company.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), CompanyModule],
-  controllers: [AppController, CompanyController],
-  providers: [AppService, CompanyService],
+  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
