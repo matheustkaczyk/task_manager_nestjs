a261e968a56aaa669c3b5ecfd3fe3e7d8a75d47e
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 10:59:51 -0300

    feat: criado metodo para criação da company

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { Model } from 'mongoose';
+import { CompanyInformationDto } from './dto/company-information.dto';
 
 @Injectable()
 export class CompanyService {
   constructor(@InjectModel(Company.name) private companyModel: Model<CompanyDocument>) {}
 
-  async createCompany(): Promise<void> {
-    
+  async createCompany(createCompanyDto: CompanyInformationDto): Promise<string> {
+    try {
+      const createdCompany = new this.companyModel(createCompanyDto);
+      createdCompany.save();
+      return "Created!";
+    } catch (error) {
+      return error.message;
+    }
   }
 }
