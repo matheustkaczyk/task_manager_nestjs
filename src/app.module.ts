c6d1187a70a99efd0ebf180e0d8d3d6818bcbb9d
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 14:53:20 -0300

    feat: gerando resource para user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from './company/company.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
