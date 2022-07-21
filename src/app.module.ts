d0517d919ffcb4856d9b307066b3cebfee4ee932
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 10:39:30 -0300

    feat: gerando company module e adicionando os imports do schema company

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CompanyController } from './company/company.controller';
 import { CompanyService } from './company/company.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), CompanyModule],
   controllers: [AppController, CompanyController],
   providers: [AppService, CompanyService],
 })
