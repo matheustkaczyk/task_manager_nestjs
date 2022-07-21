4245e724f1ed29f53cc3db706cea8b0f07c38b36
matheustkaczyk <matheus111904@hotmail.com>
2022-07-21 10:21:50 -0300

    feat: gerando service para a rota company

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyController } from './company/company.controller';
+import { CompanyService } from './company/company.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CompanyController],
-  providers: [AppService],
+  providers: [AppService, CompanyService],
 })
 export class AppModule {}
