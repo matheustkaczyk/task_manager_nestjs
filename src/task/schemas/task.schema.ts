86f748da4064aa3315b5f9c12651011631ebab76
matheustkaczyk <matheus111904@hotmail.com>
2022-07-22 10:14:09 -0300

    feat: estruturando schema das tasks

---
+++ src/task/schemas/task.schema.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TaskDocument = Task & Document;
+
+@Schema()
+export class Task {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  accountable: string[];
+
+  @Prop({ required: true })
+  deliveryDate: Date;
+
+  @Prop({ required: true })
+  status: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(Task);
