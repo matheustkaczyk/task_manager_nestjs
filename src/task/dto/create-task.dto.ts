a89ac2d78497af0fc639182cfc80afd88c60bd2f
matheustkaczyk <matheus111904@hotmail.com>
2022-07-22 09:37:13 -0300

    feat: configurando dto da criacao de task

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,12 @@
-export class CreateTaskDto {}
+import { IsString, IsDate } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  name: string;
+
+  @IsString({ each: true })
+  accountable: string[];
+
+  @IsDate()
+  deliveryDate: Date;
+}
