05f351a254993a67ca9e8df0789a7f9c30be0296
matheustkaczyk <matheus111904@hotmail.com>
2022-07-23 11:45:11 -0300

    fix: removendo arquivos inuteis gerados na criacao do projeto nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from './company/company.module';
 import { UserModule } from './user/user.module';
@@ -8,7 +6,5 @@ import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule, UserModule, TaskModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
