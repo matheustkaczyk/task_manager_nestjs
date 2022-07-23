5821de6bc1b401f9e0c5509ccdb44cc6df0eb169
matheustkaczyk <matheus111904@hotmail.com>
2022-07-23 11:52:29 -0300

    feat: gerando estrutura para auth, exportando userService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CompanyModule } from './company/company.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule, UserModule, TaskModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule, UserModule, TaskModule, AuthModule],
 })
 export class AppModule {}
