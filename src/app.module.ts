e544e080141b584b330a0cac9d8336a3c9b74c5d
matheustkaczyk <matheus111904@hotmail.com>
2022-07-26 10:58:04 -0300

    refat: alterando app module para contemplar uri do db nas variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { CompanyModule } from './company/company.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/workspace'), CompanyModule, UserModule, TaskModule, AuthModule],
+  imports: [MongooseModule.forRoot(process.env.DB || 'mongodb://localhost/workspace'), CompanyModule, UserModule, TaskModule, AuthModule],
 })
 export class AppModule {}
